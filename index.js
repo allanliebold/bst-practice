4b6ec0dc57b113bbb2f9925c5c9cb009af732e4f
Allan Liebold <allan.liebold@gmail.com>
2017-09-21 13:27:19 -0700

    tree contains method finally works

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 function Node(val) {
   this.value = val;
   this.left = null;
@@ -36,6 +38,8 @@ BinarySearchTree.prototype.push = function(val) {
   }
 }
 
+
+
 var bst = new BinarySearchTree();
 bst.push(50);
 bst.push(22);
@@ -46,6 +50,7 @@ bst.push(124);
 bst.push(2);
 bst.push(92);
 bst.push(88);
+
 
 function dfs(node){
   if(node){
@@ -80,9 +85,8 @@ function postorder(node){
 }
 
 function findMin(node){
-  if(!node){
-    return 0;
-  }
+  if(!node) return 0;
+
   if(node.left){
     return findMin(node.left);
   }
@@ -90,9 +94,8 @@ function findMin(node){
 }
 
 function findMax(node){
-  if(!node){
-    return 0;
-  }
+  if(!node) return 0;
+
   if(node.right){
     return findMax(node.right);
   }
@@ -107,15 +110,53 @@ function findHeight(node){
   return Math.max(leftHeight, rightHeight) + 1;
 }
 
-console.log('DFS:');
-dfs(bst.root);
-console.log('Inorder:');
-inorder(bst.root);
-console.log('Preorder:');
-preorder(bst.root);
-console.log('Postorder:');
-postorder(bst.root);
-console.log('Min:', findMin(bst.root));
-console.log('Max:', findMax(bst.root));
+function treeContains(node, num){
+  if(node.value === num) {
+    return true;
+  }
 
-console.log('Height:', findHeight(bst.root));
+  if(node.value > num) {
+    if(node.left != null) {
+      return treeContains(node.left, num);
+    } else {
+      return false;
+    }
+  } else if(node.right != null) {
+      return treeContains(node.right, num);
+    } else {
+      return false;
+    }
+}
+
+function findNearest(node, num){
+  var current = node.value;
+  console.log('current:', current);
+  var next = num < current ? node.left : node.right;
+  if(next != null) {
+    var next2 = findNearest(next, num);
+  }
+
+  return current;
+
+  console.log();
+  return Math.abs(current - num) < Math.abs(next - num) ? current : next;
+}
+
+// console.log('DFS:');
+// dfs(bst.root);
+// console.log('Inorder:');
+// inorder(bst.root);
+// console.log('Preorder:');
+// preorder(bst.root);
+// console.log('Postorder:');
+// postorder(bst.root);
+// console.log('Min:', findMin(bst.root));
+// console.log('Max:', findMax(bst.root));
+//
+// console.log('Height:', findHeight(bst.root));
+
+// console.log('Nearest to 1:', findNearest(bst.root, 1));
+
+console.log('Tree contains 50?:', treeContains(bst.root, 50));
+console.log('Tree contains 55?:', treeContains(bst.root, 55));
+console.log('Tree contains 28?:', treeContains(bst.root, 28));
